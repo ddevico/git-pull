d76edccfc65dac3a7444273f3205e1a7f6f89461
Davy Devico <ddevico@student.42.fr>
2018-08-06 15:57:25 +0200

    Update git-pull.sh

--- git-pull.sh
+++ git-pull.sh
@@ -4,7 +4,7 @@
 #description     :This script will make a git fetch & git pull for a git branch.
 #author		 :Davy Devico
 #date            :2018-08-03
-#version         :1.1
+#version         :1.2
 #usage		 :sh git-pull.sh
 #notes           :Install gr to use this script.
 #==============================================================================
@@ -17,7 +17,12 @@ function changeNameGlobal()
 	NEW_VALUE="\"$1\""
 	NAME_GLOBAL=$2
 	sed_param=s/${NAME_GLOBAL}=\".*/${NAME_GLOBAL}=${NEW_VALUE}/
-	sed -i $SCRIPTPATH/git-pull.sh -e "$sed_param"
+	echo "foo" | sed -i $SCRIPTPATH/git-pull.sh -e "$sed_param" 2> /dev/null
+	return=$?
+	if [[ $return != 0 ]]; then
+			echo -e "An error has occurred"
+			exit
+	fi
 }
 
 function getNameOfBranch()
@@ -25,64 +30,39 @@ function getNameOfBranch()
 	arr=("$@")
 	tLen=${#arr[@]}
 	branch=${arr[$i]##*=}
-	changeNameGlobal "$branch" "BRANCH"
 }
 
 function getMaxFromBranch()
 {
 	arr=("$@")
-	for ((i=0; i<$tLen; i+=1))
+	tlen=${#arr[@]}
+	for ((i=0; i<$tlen; i+=1))
 	do
-		tab[$i]=${arr[$i]##*-}
+		tab2[$i]=${arr[$i]##*-}
 	done
-	max=${tab[0]}
-	for n in "${tab[@]}" ; do
-	    ((n > max)) && max=$n
+	max=${tab2[0]}
+	for n in "${tab2[@]}" ; do
+		if [[ n > max ]]; then
+			max=$n
+		fi
 	done
 }
 
-SCRIPT="$(readlink --canonicalize-existing "$0")"
-SCRIPTPATH="$(dirname "$SCRIPT")"
-cd $SCRIPTPATH
-
-if [ ! -z "$1" ]; then
-	if [[ $1 = '--help' ]]; then
-		if [[ $OPTION = "auto" ]]; then
-			echo -e "You are in automatic mode.\n\nTo switch to manual mode, use the -manual='name of branch' option :\n./git-pull -manual='name of branch'"
-		else
-			echo -e "You are in manual mode on "$BRANCH"\n\nTo switch to auto mode, use the -auto option :\n./git-pull -auto"
+function autoMode()
+{
+	symbolic=$(gr git symbolic-ref HEAD --short 2> /dev/null)
+	i=0
+	for ligne in $symbolic; do
+		if [ $ligne != "in" ]; then
+			tab[$i]=$ligne
+			i=$((i + 1))
 		fi
-		exit
-	elif [[ $1 =~ ^-manual=[[:print:]]+ ]]; then
-		changeNameGlobal "manual" "OPTION"
-		getNameOfBranch "$1"
-		echo -e "Manual mode enabled\nyou have selected : "$branch
-		exit
-	elif [[ $1 = '-auto' ]]; then
-		changeNameGlobal "auto" "OPTION"
-		changeNameGlobal "" "BRANCH"
-		echo "Auto mode enabled"
-		exit
-	else
-		echo -e "This option does not exist.\n-help for more informations"
-		exit
-	fi
-fi
-
-symbolic=$(gr git symbolic-ref HEAD --short 2> /dev/null)
-i=0
-for ligne in $symbolic; do
-	if [ $ligne != "in" ]; then
-		tab[$i]=$ligne
-		i=$((i + 1))
-	fi
-done
-tlen=${#tab[@]}
-
-if [[ $option = "auto" ]]; then
+	done
+	tLen=${#tab[@]}
+	
 	trbInc=0
 	releaseInc=0
-	for ((i=2; i<$tlen; i+=3))
+	for ((i=2; i<$tLen; i+=3))
 	do
 		if [[ ${tab[$i]} =~ ^trb-[a-z]{3}-release-[0-9]{4}$ ]]; then
 			trb[$trbInc]=${tab[$i]}
@@ -101,26 +81,69 @@ if [[ $option = "auto" ]]; then
 			getMaxFromBranch "${release[@]}"
 			echoBranch="release-"$max
 			BRANCH=$echoBranch
-			echo $BRANCH
 		fi
 	else
 		getMaxFromBranch "${trb[@]}"
 		echoBranch="trb-bbt-release-"$max
 		BRANCH=$echoBranch
-		echo $BRANCH
+	fi
+}
+
+SCRIPT="$(readlink --canonicalize-existing "$0")"
+SCRIPTPATH="$(dirname "$SCRIPT")"
+cd $SCRIPTPATH
+
+if [ ! -z "$1" ]; then
+	if [[ $1 = '--help' ]]; then
+		if [[ $OPTION = "auto" ]]; then
+			autoMode
+			echo -e "You are in \033[0;36mauto mode\033[0m on the branch \033[0;33m"$BRANCH"\033[0m\n\nTo switch to manual mode, use the -manual='name of branch' option :\n./git-pull -manual='name of branch'"
+		else
+			echo -e "You are in\033[0;36m manual mode\033[0m on the branch \033[0;33m"$BRANCH"\033[0m\n\nTo switch to auto mode, use the -auto option :\n./git-pull -auto"
+		fi
+		exit
+	elif [[ $1 =~ ^-manual=[[:print:]]+ ]]; then
+		changeNameGlobal "manual" "OPTION"
+		getNameOfBranch "$1"
+		changeNameGlobal "$branch" "BRANCH"
+		echo -e "\033[0;36mManual mode\033[0m enabled\n\nyou have selected : \033[0;33m"$branch"\033[0m"
+		exit
+	elif [[ $1 = '-auto' ]]; then
+		changeNameGlobal "auto" "OPTION"
+		changeNameGlobal "" "BRANCH"
+		autoMode
+		echo -e "\033[0;36mAuto mode\033[0m enabled\n\nthe branch that will be pull is :\033[0;33m"$BRANCH"\033[0m"
+		exit
+	else
+		echo -e "This option does not exist.\n--help for more informations"
+		exit
 	fi
 fi
 
-printf "\n______________ GIT CHECKOUT TRB_BBT ______________\n"
+symbolic=$(gr git symbolic-ref HEAD --short 2> /dev/null)
+i=0
+for ligne in $symbolic; do
+	if [ $ligne != "in" ]; then
+		tab[$i]=$ligne
+		i=$((i + 1))
+	fi
+done
+tLen=${#tab[@]}
+
+if [[ $OPTION = "auto" ]]; then
+	autoMode
+fi
+
+printf "\033[0;33m\n__________________ GIT CHECKOUT TRB_BBT __________________\n"
 gr git checkout $BRANCH
-printf "\n___________________ GIT FETCH ____________________\n"
+printf "\033[0;33m\n_______________________ GIT FETCH ________________________\n"
 gr git fetch
-printf "\n____________ GIT PULL ORIGIN TRB_BBT _____________\n"
+printf "\033[0;33m\n________________ GIT PULL ORIGIN TRB_BBT _________________\n"
 gr git pull origin $BRANCH
-printf "\n__________________ RETURN BRANCH _________________\n\n"
+printf "\033[0;33m\n______________________ RETURN BRANCH _____________________\033[0m\n\n"
 
 j=2
-for ((i=1; i<$tlen; i+=3))
+for ((i=1; i<$tLen; i+=3))
 do
 	path=$(basename ${tab[$i]})
 	path=`echo $path | sed -r "s/\x1B\[([0-9]{1,2}(;[0-9]{1,2})?)?[m|K]//g"`
